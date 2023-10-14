54437e2f1805423839e75108ba350ed148339208
mofuleo <masakoterui12@gmail.com>
2023-10-15 00:30:11 +0900

    プロトタイプ情報の投稿機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
-  resources :prototypes, only: :index
+  resources :prototypes, only: [:index, :new, :create]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
