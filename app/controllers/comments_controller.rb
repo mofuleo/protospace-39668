8cb794625d4706780661948aacff8cc99fc3c4a5
mofuleo <masakoterui12@gmail.com>
2023-10-17 20:20:47 +0900

    comment 保存

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,22 @@
 class CommentsController < ApplicationController
 
   def create
+    @prototype = Prototype.find(params[:prototype_id])
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to prototype_comments_path(@prototype)
+    else
+      # Rails.logger.error(@comment.errors.full_messages.join(", "))
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
+      render :prototypes/show, status: :unprocessable_entity
+    end
   end
-  
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id:params[:prototype_id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -77,5 +77,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_16_102531) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "prototypes"
+  add_foreign_key "comments", "users"
   add_foreign_key "prototypes", "users"
 end
