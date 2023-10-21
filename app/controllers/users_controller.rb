a910c5007a26626e7798d8af335b47ee43b5e507
mofuleo <masakoterui12@gmail.com>
2023-10-21 12:48:10 +0900

    ログインしていなくても詳細ページが見れる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-before_action :move_to_index, except:[:index]
+before_action :move_to_index, except:[:index,:show]
 
   def show
     @user = User.find(params[:id])
